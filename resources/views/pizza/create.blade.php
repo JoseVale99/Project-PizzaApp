49bb956018cc6661eb3b6b50cb77ac8655943761
Zarate Carreno Jose Valentin <lc19160007@oaxaca.tecnm.mx>
2021-10-21 12:34:39 -0500

    📱 Design:  for view Create

--- resources/views/pizza/create.blade.php
+++ resources/views/pizza/create.blade.php
@@ -14,54 +14,71 @@
                         </ul>
                     </div>
                 </div>
+                @if (count($errors) > 0)
+                    <div class="card mt-5">
+                        <div class="card-body">
+                            <div class="alert alert-danger">
+                                @foreach ($errors->all() as $error)
+                                    <li class="p-6">{{ $error }}</li>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                @endif
             </div>
 
             <div class="col-md-8">
                 <div class="card">
+
                     <div class="card-header">{{ __('Pizza') }}</div>
-                <form action="{{route('pizza.store')}}" method="POST">
-                    @csrf
-                    <div class="card-body">
-                        <div class="form-group ">
-                            <label for="name">Name of Pizza</label>
-                            <input type="text" class="form-control" name="name" placeholder="name of pizza">
+
+
+                    <form action="{{ route('pizza.store') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="card-body">
+                            <div class="form-group ">
+                                <label for="name">Name of Pizza</label>
+                                <input type="text" class="form-control" name="name" placeholder="name of pizza">
+                            </div>
+
+                            <div class="form-group ">
+                                <label for="description">Description of Pizza</label>
+                                <textarea class="form-control" name="description"></textarea>
+                            </div>
+
+                            <div class="form-group">
+
+                                <label>Pizza Price ($)</label>
+                                <input type="number" name="small_pizza_price" class="formm-control mt-3"
+                                    placeholder="small pizza price">
+                                <input type="number" name="medium_pizza_price" class="form-control mt-3"
+                                    placeholder="medium pizza price">
+                                <input type="number" name="large_pizza_price" class="form-control mt-3"
+                                    placeholder="large pizza price">
+
+                            </div>
+
+                            <div class="form-group ">
+                                <label for="description">Category</label>
+                                <select class="form-control" name="category" id="">
+                                    <option value=""></option>
+                                    <option value="vegetarian">Vegetarian Pizza</option>
+                                    <option value="nonvegetarian">Non vegetarian Pizza</option>
+                                    <option value="traditional">Traditional Pizza</option>
+                                </select>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="image">Image</label>
+                                <input type="file" class="form-control" name="image">
+                            </div>
+
+                            <div class="form-group text-center">
+                                <button class="btn btn-primary" type="submit">save</button>
+                            </div>
+
                         </div>
-
-                        <div class="form-group ">
-                            <label for="description">Description of Pizza</label>
-                            <textarea class="form-control" name="description"></textarea>
-                        </div>
-
-                        <div class="form-group">
-
-                            <label>Pizza Price ($)</label>
-                            <input type="number" name="small_pizza_price" class="formm-control mt-3" placeholder="small pizza price">
-                            <input type="number" name="medium_pizza_price" class="form-control mt-3" placeholder="medium pizza price">
-                            <input type="number" name="large_pizza_price" class="form-control mt-3" placeholder="large pizza price">
-
-                        </div>
-
-                        <div class="form-group ">
-                            <label for="description">Category</label>
-                            <select class="form-control" name="category" id="">
-                                <option value=""></option>
-                                <option value="vegetarian">Vegetarian Pizza</option>
-                                <option value="nonvegetarian">Non vegetarian Pizza</option>
-                                <option value="traditional">Traditional Pizza</option>
-                            </select>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="image">Image</label>
-                            <input type="file" class="form-control" name="image">
-                        </div>
-
-                        <div class="form-group text-center">
-                            <button class="btn btn-primary" type="submit">save</button>
-                        </div>
-
-                    </div>
-                </form>
+                    </form>
 
                 </div>
             </div>
