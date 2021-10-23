d7a910b3df8773c6a418a37007146de29dd44534
Zarate Carreno Jose Valentin <lc19160007@oaxaca.tecnm.mx>
2021-10-23 16:31:18 -0500

    💥 NEW: route  update pizza

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,4 @@ Route::get('/pizza', [PizzaController::class, 'index'])->name('pizza.index');
 Route::get('/pizza/create', [PizzaController::class, 'create'])->name('pizza.create');
 Route::post('/pizza/store', [PizzaController::class, 'store'])->name('pizza.store');
 Route::get('/pizza/{id}/edit', [PizzaController::class, 'edit'])->name('pizza.edit');
+Route::put('/pizza/{id}/update', [PizzaController::class, 'update'])->name('pizza.update');
