eeecd3b95d1a9ad99128f0f633105aea9936f4fb
Zarate Carreno Jose Valentin <lc19160007@oaxaca.tecnm.mx>
2021-10-26 00:55:18 -0500

    💥 NEW: create user Admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $user  = new User();
+        $user->name = 'Admin';
+        $user->email = 'admin@example.com';
+        $user->password = bcrypt('1234567890');
+        $user->is_admin = 1;
+        $user->save();
     }
 }
