1e683a7260aa418df00629cadac3bb23f04da582
Zarate Carreno Jose Valentin <lc19160007@oaxaca.tecnm.mx>
2021-10-26 01:51:19 -0500

    💥 NEW: desing for pizza index

--- resources/views/pizza/index.blade.php
+++ resources/views/pizza/index.blade.php
@@ -3,12 +3,42 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-12">
+            <div class="col-md-2">
+                <div class="card">
+                    <div class="card-header">{{ __('Menu') }}</div>
+
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <a href="{{ route('pizza.index') }}" class="list-group-item list-group-item-action">View</a>
+                            <a href="{{ route('pizza.create') }}" class="list-group-item list-group-item-action">Create</a>
+                            <a href="{{ route('user.order') }}" class="list-group-item list-group-item-action">User order</a>
+
+                        
+                        </ul>
+                    </div>
+                </div>
+                @if (count($errors) > 0)
+                    <div class="card mt-5">
+                        <div class="card-body">
+                            <div class="alert alert-danger">
+                                @foreach ($errors->all() as $error)
+                                    <li class="p-6">{{ $error }}</li>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                @endif
+            </div>
+
+
+            <div class="col col-md-10">
                 <div class="card">
                     <div class="card-header">{{ __('All Pizza') }}</div>
-                       <a href="{{route('pizza.create')}}">
-                        <button class="btn btn-success" style="float :right;" >Add Pizza</button>
-                       </a>
+                    <div class="text-right mt-3 mx-3">
+                        <a href="{{ route('pizza.create') }}">
+                            <button class="btn btn-success">Add Pizza</button>
+                        </a>
+                    </div>
                     <div class="card-body">
                         @if (session('message'))
                             <div class="alert alert-success" role="alert">
@@ -53,33 +83,34 @@
                                                 data-target="#exampleModal{{ $pizza->id }}" type="button"><img
                                                     src="https://img.icons8.com/external-kiranshastry-solid-kiranshastry/20/000000/external-delete-miscellaneous-kiranshastry-solid-kiranshastry.png" /></button>
                                             <!-- Modal -->
-                                            <div class="modal fade" id="exampleModal{{$pizza->id}}" tabindex="-1"
+                                            <div class="modal fade" id="exampleModal{{ $pizza->id }}" tabindex="-1"
                                                 aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                                <form action="{{route('pizza.destroy', $pizza->id)}}" method="POST">
-                                                @csrf
-                                                @method('DELETE')
-                                                <div class="modal-dialog">
-                                                    <div class="modal-content">
-                                                        <div class="modal-header">
-                                                            <h5 class="modal-title" id="exampleModalLabel">Delete confirmation
-                                                            </h5>
-                                                            <button type="button" class="close"
-                                                                data-dismiss="modal" aria-label="Close">
-                                                                <span aria-hidden="true">&times;</span>
-                                                            </button>
-                                                        </div>
-                                                        <div class="modal-body">
-                                                         Are you sure to eliminate this product?
-                                                        </div>
-                                                        <div class="modal-footer">
-                                                            <button type="button" class="btn btn-secondary"
-                                                                data-dismiss="modal">Close</button>
-                                                            <button type="submit" class="btn btn-danger">Delete
+                                                <form action="{{ route('pizza.destroy', $pizza->id) }}" method="POST">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <div class="modal-dialog">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <h5 class="modal-title" id="exampleModalLabel">Delete
+                                                                    confirmation
+                                                                </h5>
+                                                                <button type="button" class="close"
+                                                                    data-dismiss="modal" aria-label="Close">
+                                                                    <span aria-hidden="true">&times;</span>
                                                                 </button>
+                                                            </div>
+                                                            <div class="modal-body">
+                                                                Are you sure to eliminate this product?
+                                                            </div>
+                                                            <div class="modal-footer">
+                                                                <button type="button" class="btn btn-secondary"
+                                                                    data-dismiss="modal">Close</button>
+                                                                <button type="submit" class="btn btn-danger">Delete
+                                                                </button>
+                                                            </div>
                                                         </div>
                                                     </div>
-                                                </div>
-                                            </form>
+                                                </form>
                                             </div>
                                         </td>
                                     </tr>
@@ -88,10 +119,12 @@
                                 @endforelse
                             </tbody>
                         </table>
-                        {{$pizzas->links()}}
+                        {{ $pizzas->links() }}
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
+
 @endsection
