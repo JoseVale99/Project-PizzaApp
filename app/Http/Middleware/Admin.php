ce2765b1642d0557733e4861bcbf73ddd53965bf
Zarate Carreno Jose Valentin <lc19160007@oaxaca.tecnm.mx>
2021-11-02 19:03:00 -0600

    🚀 RELEASE: new version for to admin

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -16,11 +16,9 @@ class Admin
      */
     public function handle(Request $request, Closure $next)
     {
-        if (auth()->user()->is_admin ==1){
-             return $next($request);
+        if(auth()->user()->is_admin == 1){
+            return $next($request);
         }
-
         return redirect('login');
-       
     }
 }
